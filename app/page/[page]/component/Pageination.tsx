56204ddebf5c2712091094c9b4cb4cfd253ead1e
meanboy20 <johnnyyu0320@gmail.com>
2023-06-24 16:03:37 -0700

    Testing render pagination based to window size listener

--- app/page/[page]/component/Pageination.tsx
+++ app/page/[page]/component/Pageination.tsx
@@ -1,5 +1,3 @@
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
-
 type PaginationProps = {
   totalPages: number;
   currentPage: number;
@@ -77,7 +75,6 @@ export default function Pageination({
               className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Previous</span>
-              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
             </a>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
             {renderButtons()}
@@ -87,7 +84,6 @@ export default function Pageination({
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Next</span>
-              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
             </a>
           </nav>
         </div>
